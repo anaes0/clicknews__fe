807e7ae8c4d0ad7a997e6e403cfc3da30ec19700
anaes0 <anaesvg@gmail.com>
2023-02-07 09:55:12 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../src/components/common/header/header";
+
+/*
+To run this App you need to install:  
+- npm install react-router-dom@6
+- npm install react-icons/fa
+*/
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/common/header/head.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function head() {
+  return (
+    <>
+      <section className="head">
+        <div className="container flexSB paddingTB">
+          <div className="logo">
+            <img src="../images/logo.png" alt="" />
+          </div>
+          <div className="login">
+            <img src="../images/headerb.png" alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default head;
