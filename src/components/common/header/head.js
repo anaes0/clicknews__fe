4b9079348c2e204af8a94f77073d88a2e385c792
anaes0 <anaesvg@gmail.com>
2023-02-07 10:22:55 +0000

    first commit

--- src/components/common/header/head.js
+++ src/components/common/header/head.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegUserCircle } from "react-icons/fa";
 
 function head() {
   return (
@@ -9,7 +10,19 @@ function head() {
             <img src="../images/logo.png" alt="" />
           </div>
           <div className="login">
-            <img src="../images/headerb.png" alt="" />
+            <button className="login-btn">
+              <FaRegUserCircle
+                className="login-icon"
+                style={{
+                  position: "absolute",
+                  top: "18px",
+                  right: "45px",
+                }}
+                size="28px"
+                color="white"
+              />
+              <h4>Sign in</h4>
+            </button>
           </div>
         </div>
       </section>
